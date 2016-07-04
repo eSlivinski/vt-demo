332010a582153ba50f5832c1d35bf4372608dad2
eSlivinski <eslivinski@gmail.com>
2016-07-04 14:26:23 -0400

    initial commit

---
+++ www/public_src/scripts/main.js
@@ -0,0 +1,13 @@
+angular.module('vt-demo', ['ui.router'])
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+      .state('homepage', {
+        url: '/',
+        templateUrl: 'views/pages/homepage.html',
+        controller: function () {
+          console.log('Hello');
+        }
+      });
+  });
