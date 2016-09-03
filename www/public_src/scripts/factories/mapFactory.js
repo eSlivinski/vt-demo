7ad60fc457e2ae7fb12504ce0753517e97417038
eSlivinski <eslivinski@gmail.com>
2016-09-03 19:03:10 -0400

    editable

--- www/public_src/scripts/factories/mapFactory.js
+++ www/public_src/scripts/factories/mapFactory.js
@@ -2,40 +2,11 @@ var map = {},
     mapOptions = {
       container: 'map',
       style: 'mapbox://styles/mapbox/light-v9'
-    };
+    },
+    Draw = mapboxgl.Draw({displayControlsDefault : false}),
+    poles = {};
 
 var options = {
-  countryFills: {
-    'id': 'country-fills',
-    'type': 'fill',
-    'source': 'countries',
-    'layout': {},
-    'paint': {
-      'fill-color': '#627BC1',
-      'fill-opacity': 0.5
-    }
-  },
-  countryBorders: {
-    'id': 'country-borders',
-    'type': 'line',
-    'source': 'countries',
-    'layout': {},
-    'paint': {
-      'line-color': '#627BC1',
-      'line-width': 2
-    }
-  },
-  routeHover: {
-    'id': 'route-hover',
-    'type': 'fill',
-    'source': 'countries',
-    'layout': {},
-    'paint': {
-      'fill-color': '#627BC1',
-      'fill-opacity': 1
-    },
-    'filter': ['==', 'name', '']
-  },
   poleMarkers: {
     'id': 'pole-markers',
     'type': 'circle',
@@ -47,7 +18,8 @@ var options = {
         'base': 1.75,
         'stops': [[12, 2], [22, 180]]
       }
-    }
+    },
+    'filter': ['none']
   },
   poleHighlight: {
     'id': 'pole-highlight',
@@ -68,73 +40,97 @@ var options = {
 angular.module('vt-demo')
   .factory('mapFactory', function($rootScope) {
 
-    return {
+    var factory = window.factory = {
 
       initMap: function() {
+        var self = this;
         mapboxgl.accessToken = 'pk.eyJ1IjoiZXNsaXZpbnNraWNhcnRvIiwiYSI6IjRWenpDcmMifQ.IU9qcKhUf_w-lTQQ-I7DIg';
 
         map = window.map = new mapboxgl.Map(mapOptions);
         map.flying = false;
-        map.on('moveend', function(e){
+        map.editing = false;
+        map.on('moveend', function(event) {
           if (this.flying) { map.fire('flyend'); }
         });
-        // map.on('mousemove', function (e) {
-        //   var features = map.queryRenderedFeatures(e.point, { layers: ['country-fills'] });
-        //   if (features.length) {
-        //     map.setFilter('route-hover', ['==', 'name', features[0].properties.name]);
-        //   } else {
-        //     map.setFilter('route-hover', ['==', 'name', '']);
-        //   }
-        // });
-        //
-        // // Reset the route-hover layer's filter when the mouse leaves the map
-        // map.on('mouseout', function () {
-        //   map.setFilter('route-hover', ['==', 'name', '']);
-        // });
-        //
-        map.on('mousedown', function (e) {
-          var features = map.queryRenderedFeatures(e.point, { layers: ['pole-markers'] });
 
-          if (features.length) {
-            map.setFilter('pole-highlight', ['==', 'publicid', features[0].properties.publicid]);
-          } else {
-            map.setFilter('pole-highlight', ['==', 'publicid', '']);
+        map.on('mousedown', function (event) {
+          map.setFilter('pole-highlight', ['==', 'publicid', '']);
+
+          var features = map.queryRenderedFeatures(event.point, { layers: ['pole-markers'] });
+
+          if (!features.length) { return; }
+
+          var feature = _.head(features),
+              featureId = _.get(feature, 'properties.publicid');
+
+          if (map.editing) {
+            var hidden = _.cloneDeep(map.getFilter('pole-markers'));
+            hidden[0] = 'none';
+            hidden.push(['==', 'publicid', featureId]);
+            map.setFilter('pole-markers', hidden);
+            return self.edit(feature, featureId);
           }
+          map.setFilter('pole-highlight', ['==', 'publicid', featureId]);
         });
 
         map.on('load', function() {
+          map.addControl(Draw);
           $rootScope.$broadcast('map:loaded');
         });
-      },
-
-      addCountries: function(geoJson) {
-        map.addSource('countries', {
-          'type': 'geojson',
-          'data': geoJson
-        });
-
-        map.addLayer(options.countryFills);
-        map.addLayer(options.countryBorders);
-        map.addLayer(options.routeHover);
       },
 
       addPoles: function(geoJson) {
         map.flying = true;
         map.flyTo({
           center: { lng: -82.97113458848199, lat: 39.974203933947024 },
-          zoom: 9.5
+          zoom: 11
         });
-        map.once('flyend', function() {
-          map.addSource('poles', {
-            'type': 'geojson',
-            'data': geoJson
-          });
-
+        map.once('flyend', function(event) {
+          poles = new mapboxgl.GeoJSONSource({ 'data': geoJson });
+          map.addSource('poles', poles);
           map.addLayer(options.poleMarkers);
           map.addLayer(options.poleHighlight);
-
         });
-      }
+      },
 
+      update: function() {
+        var drawCollection = Draw.getAll(),
+            updateCollection = poles._data;
+
+        _.each(drawCollection.features, function(drawFeature) {
+          drawFeature.properties.publicid = drawFeature.id;
+          updateCollection.features.push(drawFeature);
+        });
+
+        Draw.deleteAll();
+        poles.setData(updateCollection);
+      },
+
+      toggleEditing: function (value) {
+        map.editing = value;
+      },
+
+      draw: function(geomType) {
+        var mode = geomType === 'line' ? 'draw_line_string' :
+            geomType === 'point' ? 'draw_point' :
+            geomType === 'polygon' ? 'draw_polygon' : false;
+
+        if (mode) {
+          Draw.changeMode(mode);
+          $rootScope.$broadcast('draw:start', { geomType: geomType });
+        }
+      },
+
+      edit: function (feature, featureId) {
+        var geom = feature.toJSON().geometry;
+        // Reduce coord precision
+        geom.coordinates = _.map(feature.geometry.coordinates, function(coord) {
+          return _.round(coord, 5);
+        });
+        geom.id = featureId;
+        Draw.add(geom);
+      }
     };
+
+    return factory;
   });
